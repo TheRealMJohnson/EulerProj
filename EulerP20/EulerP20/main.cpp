5cda23a0d7c3bbbb634622d37b4eb2005dea0a95
Matthew Johnson <m45johns@edu.uwaterloo.ca>
2018-11-10 22:00:56 -0500

    Started P20, adding DataStructure submodule before finish

---
+++ EulerP20/EulerP20/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EulerP20
+//
+//  Created by Matthew Johnson on 2018-11-10.
+//  Copyright © 2018 Matthew Johnson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
